de5bf0d0f5aae23e58766040c2e3d01d925a4a0c
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-06-02 14:16:16 +0600

    JWT route setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -13,7 +14,23 @@ app.get('/', (req, res) => {
     res.send('Hello Bistro Boss!')
 });
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized assess" });
+    }
+    const token = authorization.split('')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorize access' })
+        }
+        req.decoded = decode;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pmhks6f.mongodb.net/?retryWrites=true&w=majority`
+// console.log(process.env.ACCESS_TOKEN_SECRET);
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -30,6 +47,12 @@ async function run() {
         const bistroCollection = client.db("bistroDB").collection("menu");
         const reviewsCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("carts");
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
         // users related
         app.get("/users", async (req, res) => {
@@ -50,7 +73,6 @@ async function run() {
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
@@ -73,12 +95,18 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
 
             if (!email) {
                 res.send([]);
             }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
